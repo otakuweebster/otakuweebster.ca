7f16ea690dc1bf9d1b73a3f9c4db985c79d9ab8e
Gabriel Beltran <gabrielaudbeltran23@gmail.com>
2022-08-30 22:18:07 -0600

    Latest commit that allows the banner to slide up when going to other pages

--- src/script/index_script.js
+++ src/script/index_script.js
@@ -1,5 +1,9 @@
 $(function()
 {
+    const bannerDiv = $(".welcomebanner");
+    const aboutMe = $("#aboutMe");
+    const portfolio = $("#portfolio");
+    const logo = $("#logo");
 
     setTimeout(clearSplash, 4000);
     setTimeout(fadeCopyright, 1500);
@@ -14,4 +18,36 @@ $(function()
     {
         $("#copyright").fadeOut(1000);
     }
+
+    function bannerAnimate()
+    {
+        if (bannerDiv.is(':visible'))
+        {
+            bannerDiv.slideUp(500);
+        }
+
+    }
+
+    /**
+     * Below is where, depending on the location of the page, slides up the big welcome sign banner
+     */
+    aboutMe.on("click", function () {
+        bannerAnimate();
+    });
+
+    portfolio.on("click", function () {
+        bannerAnimate();
+    })
+
+    logo.on("click", function () {
+        bannerDiv.slideDown(500);
+    })
+
+
+
+
+
+
+
+
 })
